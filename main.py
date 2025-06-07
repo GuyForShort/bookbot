0f2f6c199f5794c14628bb8474928e2c67f1d62a
GuyForShort <GiteousMaximus@icloud.com>
2025-06-07 17:39:38 +0100

    Final version of Bookbot I hope!

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from stats import * #get_num_words
-from string import *
+
 
 
 def main():
-    output={}
-    output=count_letters("./books/frankenstein.txt")
-    print(output)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    word_count=get_num_words("./books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    letter_count_dict={}
+    letter_count_dict=count_letters("./books/frankenstein.txt")
+    #print(letter_count_dict)
+    unsorted_list=convert_to_list(letter_count_dict)
+    #print(unsorted_list)
+    sorted_list=list_sorter(unsorted_list)
+    for a in sorted_list:
+        print(f"{a["char"]}: {a["count"]}")
+
 main()
--- stats.py
+++ stats.py
@@ -4,32 +4,39 @@ def get_num_words(bookpath):
     with open(bookpath) as file:
         book = file.read()
         words = book.split()
-        word_count = 0
-        for word in words:
-            word_count += 1
-
-        print(f"{word_count} words found in the document")
-    return
-
-def characters():
-    output =list(printable)
-    return output
+        word_count = len(words)
+    return word_count
 
 def count_letters(bookpath):
-    char_list=characters()
-    counted_letters={}
+
+    char_list={}
     letter_count = 0
     with open(bookpath) as file:
         book = file.read()
         lcase_book = book.lower()
         booklist = list(lcase_book)
-        for char in char_list:
-            #print(char)
-            for letter in booklist:
-                if char == letter:
-                    letter_count += 1
-            #print(letter_count)        
-            counted_letters[char]=letter_count
-            #print(counted_letters)
-            letter_count = 0
-    return counted_letters
+
+        for letter in booklist:
+            if letter.isalpha():
+                if letter in char_list:
+                    char_list[letter] += 1 
+                else:
+                    char_list[letter] = 1
+
+
+    return char_list
+
+def convert_to_list(dict_in):
+    listdict=[]
+    for letter in dict_in:
+        count=dict_in[letter]
+        listdict.append({"char": letter, "count": count})
+    return listdict
+
+def sort_on(dict):
+    return dict["count"]
+
+
+def list_sorter(unsorted_list):
+    unsorted_list.sort(reverse=True, key=sort_on)
+    return unsorted_list
